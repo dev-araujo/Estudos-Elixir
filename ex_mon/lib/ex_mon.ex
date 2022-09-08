d064297a7e498fb538c78453efea1fae1456675c
dev-araujo <theriancirculo@protonmail.com>
2022-09-08 10:00:03 -0300

    feat: agent

--- ex_mon/lib/ex_mon.ex
+++ ex_mon/lib/ex_mon.ex
@@ -1,7 +1,12 @@
 defmodule ExMon do
-alias ExMon.Player
-  
+  alias ExMon.Player
+
   def create_player(name, fight, furious, cure) do
-     Player.build(name, fight, furious, cure)
+    Player.build(name, fight, furious, cure)
+  end
+
+  def start_game(player) do
+    computer = create_player("Robotinix", :garra, :explosao, :cura_sinistra)
+    ExMon.Game.start(computer, player)
   end
 end
